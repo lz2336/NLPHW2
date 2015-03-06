5186f53cc093a3e0723758cf94731d8f2f064c38
Lingzi Zhuang <Lingzi@dyn-129-236-216-154.dyn.columbia.edu>
2015-03-06 00:45:22 -0500

    3

--- test.py
+++ test.py
@@ -6,21 +6,21 @@ from featureextractor import FeatureExtractor
 from transition import Transition
 
 if __name__ == '__main__':
-    data = dataset.get_swedish_train_corpus().parsed_sents()
+    data = dataset.get_danish_train_corpus().parsed_sents()
     random.seed(1234)
     subdata = random.sample(data, 200)
 
     try:
         tp = TransitionParser(Transition, FeatureExtractor)
         tp.train(subdata)
-        tp.save('english.model')
+        tp.save('danish.model')
 
         testdata = dataset.get_swedish_test_corpus().parsed_sents()
-        tp = TransitionParser.load('english.model')
+        tp = TransitionParser.load('danish.model')
 
         parsed = tp.parse(testdata)
 
-        with open('english.conll', 'w') as f:
+        with open('danish.conll', 'w') as f:
             for p in parsed:
                 f.write(p.to_conll(10).encode('utf-8'))
                 f.write('\n')
