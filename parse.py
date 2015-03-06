8221c6ab1a9b77527485fb39393212ca263f2a33
Lingzi Zhuang <Lingzi@dyn-160-39-148-224.dyn.columbia.edu>
2015-03-06 10:24:48 -0500

    3

--- parse.py
+++ parse.py
@@ -1,9 +1,9 @@
-from providedcode.transitionparser import TransitionParser
+from providedcode.transitionparser import Configuration, TransitionParser
 from providedcode.dependencygraph import DependencyGraph
 import sys
 
+tp = TransitionParser.load(sys.argv[1])
 for line in sys.stdin:
-	tp = TransitionParser.load(sys.argv[1])
 	sentence = DependencyGraph.from_sentence(line)
 	parsed = tp.parse([sentence])
 	print parsed[0].to_conll(10).encode('utf-8')
