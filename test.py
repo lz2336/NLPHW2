b395c62cd3f9e1d1da1773e1c93673f9b0dd4078
Lingzi Zhuang <Lingzi@dyn-160-39-148-224.dyn.columbia.edu>
2015-03-06 23:42:57 -0500

    3

--- test.py
+++ test.py
@@ -8,9 +8,9 @@ from transition import Transition
 if __name__ == '__main__':
     
     # data = dataset.get_swedish_train_corpus().parsed_sents()
-    # data = dataset.get_danish_train_corpus().parsed_sents()
+    data = dataset.get_danish_train_corpus().parsed_sents()
     # data = dataset.get_english_train_corpus().parsed_sents()
-    data = dataset.get_korean_train_corpus().parsed_sents()
+    # data = dataset.get_korean_train_corpus().parsed_sents()
 
     random.seed(1234)
     subdata = random.sample(data, 200)
@@ -20,23 +20,23 @@ if __name__ == '__main__':
         tp.train(subdata)
 
         # tp.save('swedish.model')
-        # tp.save('danish.model')
+        tp.save('danish.model')
         # tp.save('english.model')
-        tp.save('korean.model')
+        # tp.save('korean.model')
 
         # testdata = dataset.get_swedish_test_corpus().parsed_sents()
-        # testdata = dataset.get_danish_test_corpus().parsed_sents()
+        testdata = dataset.get_danish_test_corpus().parsed_sents()
         # testdata = dataset.get_english_dev_corpus().parsed_sents()
-        testdata = dataset.get_korean_test_corpus().parsed_sents()
+        # testdata = dataset.get_korean_test_corpus().parsed_sents()
         
         # tp = TransitionParser.load('badfeatures.model')
 
         parsed = tp.parse(testdata)
 
         # conllFile = 'swedish.conll'
-        # conllFile = 'danish.conll'
+        conllFile = 'danish.conll'
         # conllFile = 'english.conll'
-        conllFile = 'korean.conll'
+        # conllFile = 'korean.conll'
 
         with open(conllFile, 'w') as f:
             for p in parsed:
